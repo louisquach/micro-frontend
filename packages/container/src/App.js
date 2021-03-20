1ae7c8a61a8c23301f449ba77f72188733005a9a
Louis Quach <quachminhluantn@gmail.com>
2021-03-20 21:04:33 +0800

    changed package

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp'
 export default () => {
     return (
         <div>
-            <h1>WELCOME TO CONTAINER</h1>
+            <h1>CONTAINER</h1>
             <hr/>
             <MarketingApp/>
         </div>
