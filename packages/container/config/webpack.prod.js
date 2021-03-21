f8e848ef9609fd856f1d5ef817ff76d0faa0e34f
Louis Quach <quachminhluantn@gmail.com>
2021-03-21 16:58:21 +0800

    change prod config

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
         new FederationPlugin({
             name: 'container',
             remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
             },
             shared: package.dependencies
         })
